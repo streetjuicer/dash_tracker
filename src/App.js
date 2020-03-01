fec1d9f015d229e604fb522f7c08f9e0d15a5b05
robbie <robbiekruszynski@gmail.com>
2020-03-01 14:18:25 -0800

    nuke expensive and add incomeexp along with logic to track in vs out within balance and incomeexpenses

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
-import { Expenses } from "./components/Expenses";
+import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
   return (
     <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
-        <Expenses />
+        <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
       </div>
