0452959abd5b266b42289dda85e95efd46224465
robbie <robbiekruszynski@gmail.com>
2020-02-29 23:06:35 -0800

    add top in transaction js to dynamically render if flow is + or -

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { TransactionList } from "./TransactionList";
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <div>
-      <li className="minus">
-        {transaction.text} <span>-$400</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
         <button className="delete-btn">x</button>
       </li>
     </div>
