21911921819cea93d1865e2f59ac58d01cb491b7
robbie <robbiekruszynski@gmail.com>
2020-02-29 21:14:47 -0800

    start to add comps ans css for bones

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { Expenses } from "./components/Expenses";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <Expenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
